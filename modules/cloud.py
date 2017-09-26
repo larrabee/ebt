379b53a7129cb53cb4aed03dc500ba3752fff652
vladimir <kafatron@gmail.com>
2017-09-26 14:55:26 +0200

    Fix mistakes

--- modules/cloud.py
+++ modules/cloud.py
@@ -5,13 +5,13 @@ import boto3
 
 
 class Amazon:
-    def __init(aws_access_key_id, aws_secret_access_key, region_name):
+    def __init__(aws_access_key_id, aws_secret_access_key, region_name):
         self.log = logging.getLogger('__main__')
         self.glacier_client = boto3.client('glacier', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=region_name)
         
-    def upload file(self, vault, description, path):
+    def upload_file(self, vault, description, path):
         response = self.glacier_client.upload_archive(vaultName=vault, archiveDescription=description, body=open(path, 'rb'))
-        self.log.info('File "{0}" upload successfull. Archive id: "{1}"'.format(path, response['archiveId']))
+        self.log.debug('File "{0}" upload successfull. Archive id: "{1}"'.format(path, response['archiveId']))
         return response
 
     
